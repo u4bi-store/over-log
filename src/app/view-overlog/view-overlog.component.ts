4dbc9d411aa9dce09343dcdf50c56c66b8cba167
u4bi <u4bi@u4bi.com>
2017-01-01 02:33:09 +0900

    view overlog router render

--- src/app/view-overlog/view-overlog.component.ts
+++ src/app/view-overlog/view-overlog.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-overlog',
   templateUrl: './view-overlog.component.html',
   styleUrls: ['./view-overlog.component.css']
 })
-export class ViewOverlogComponent implements OnInit {
+export class ViewOverlogComponent implements OnInit, OnDestroy {
+  sub: any;
 
-  constructor() { }
-
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
+  
+  back() : void{
+    let link = ['/'];
+    this.router.navigate(link);
+  }
+  
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      let tag = params['tag']; 
+      let num = params['num'];
+      console.log(tag+' '+num);
+    });
   }
 
+  ngOnDestroy(){
+    this.sub.unsubscribe();
+  }
 }
